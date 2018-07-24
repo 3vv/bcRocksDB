b105ec361ac8a02810be06deb97b1dfaca5ad822
h <h@>
2018-07-24 12:27:08 +0800

    Type, Desc, Error

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -8,15 +8,15 @@ import (
 // File type
 type FileType int
 
+// File descriptor
+type FileDesc struct {
+	Type FileType
+	Num  int64
+}
+
 // Common error
 var (
 	ErrInvalidFile = errors.New(PkgName+"/storage: invalid file for argument")
 	ErrLocked      = errors.New(PkgName+"/storage: already locked")
 	ErrClosed      = errors.New(PkgName+"/storage: closed")
 )
-
-// File descriptor
-type FileDesc struct {
-	Type FileType
-	Num  int64
-}
